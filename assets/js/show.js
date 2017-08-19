76d1b6f99f5443477c81d040bbbe2ac6a4c4f1bb
pop <3064503944@qq.com>
2017-08-19 15:43:50 +0800

    详情页完成80%

--- assets/js/show.js
+++ assets/js/show.js
@@ -1,19 +1,39 @@
 $(function() {
-    $('.tiao').mousedown(function() {
-        var X = $(this).offset().top;
-        var Y = $(this).offset().left;
-        console.log($(this).position().top);
+    $('.tiao').mousedown(function(e) {
+        var Y = e.offsetY;
+        var Z = $(this).offset().top;
+        var X = Y + Z;
+        var Q = -($('.wenben2-zhu-zi').height() - $('.wenben2-zhu').height());
         $(document).mousemove(function(e) {
-            console.log(e.pageX, e.pageY);
-            var XX = e.pageX - X;
-            var YY = e.pageY - Y;
-            // console.log(YY);
-            $('.tiao').css('top', YY - 200);
+            var YY = e.pageY;
+            var b = -Q / 100;
+            $('.tiao').css('top', YY - X);
+            $('.wenben2-zhu').css('top', -b * (YY - X));
+            if (YY - X <= 0) {
+                $('.tiao').css('top', 0);
+                $('.wenben2-zhu').css('top', 0)
+            }
+            if (YY - X >= 100) {
+                $('.tiao').css('top', 100);
+                $('.wenben2-zhu').css('top', Q)
+            }
         })
         $(document).mouseup(function() {
             $(document).unbind('mousemove');
-            // console.log(111);
         })
+    })
+
+    $('.quanxuan').click(function() {
+        $('.danxuan').prop('checked', $(this).prop('checked'));
+    })
+    $('.danxuan').click(function() {
+        console.log($('.danxuan').length);
+        if ($('.danxuan:checked').length == $('.danxuan').length) {
+            $('.quanxuan').prop('checked', true);
+        } else {
+            $('.quanxuan').prop('checked', false);
+
+        }
     })
 
 })

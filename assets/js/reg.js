814a3396870e1217ddd970976a5014e3bfc67452
pop <3064503944@qq.com>
2017-08-19 19:44:53 +0800

    注册页完成45%

---
+++ assets/js/reg.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.haoma input').focus(function() {
+        $(this).next().css('font-size', '14px')
+    }).blur(function() {
+        $(this).next().css('font-size', '0')
+
+    })
+})
